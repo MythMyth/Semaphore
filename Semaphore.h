b08358220c188f3463559801c5a926127ff6f760
MythMyth <windy10091996@gmail.com>
2023-06-07 22:55:24 +0700

    Add content

--- Semaphore.h
+++ Semaphore.h
@@ -1,1 +1,38 @@
+#include<mutex>
+#include<condition_variable>
 
+class Semaphore {
+public:
+    explicit Semaphore(unsigned int desired) : res(desired){}
+    Semaphore(const Semaphore& another ) = delete;
+    Semaphore(Semaphore&& another) = delete;
+    Semaphore& operator= (const Semaphore& another) = delete;
+    Semaphore& operator= (Semaphore&& another) = delete;
+    ~Semaphore() {}
+
+    void release(const unsigned int amount = 1U) {
+        std::lock_guard lock(m_mutex);
+        res += amount;
+    }
+
+    void acquire(const unsigned int amount = 1U) {
+        std::unique_lock lock(m_mutex);
+        m_cv.wait(lock, [&]() {return res >= amount;});
+        res -= amount;
+    }
+
+    bool try_acquire(const unsigned int amount = 1U) {
+        std::lock_guard lock(m_mutex);
+        if(res >= amount) {
+            res -= amount;
+            return true;
+        }
+        return false;
+    }
+
+private:
+    std::mutex m_mutex;
+    std::condition_variable m_cv;
+    unsigned int res;
+
+};
